1041026141b9d0e195e3fff7110b2972bf5d5fd2
NULLCT <cockatiel.u10@gmail.com>
2020-04-04 14:16:39 +0100

    https://atcoder.jp/contests/abc155/submissions/11538561

---
+++ AtCoder/abc155/abc155_b.cpp
@@ -0,0 +1,39 @@
+/*//////////////////////////////////////*/
+/*/  NULLCT$ (｀･ω･´) <<"Be accept!!"  /*/
+/*//////////////////////////////////////*/
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <type_traits>
+#include <cstdint>
+#include <math.h>
+#include <queue>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#define rep(counter, limit) for (int counter = 0; counter < limit; counter++)
+typedef long long ll; using namespace std;
+int gcd(int a, int b) { if (a % b == 0) { return(b); } else { return(gcd(b, a % b)); } }
+int lcm(int a, int b) { return a * b / gcd(a, b); }
+bool primenum(int a) { if (a == 1) { return false; }if (a == 2) { return true; }if (a % 2 == 0) { return false; }for (int i = 3; i < sqrt(a) + 1; i += 2) { if (a != i) { if (a % i == 0) { return false; } } }return true; }
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+  int a;
+  cin>>a;
+  vector<int> l(a);
+  rep(i,a)cin>>l[i];
+  rep(i,a){
+    if(l[i] % 2==0){
+      if(!(l[i] % 3==0 || l[i] % 5==0)){
+        cout<<"DENIED"<<endl;
+        return 0;
+      }
+    }
+  }
+  cout<<"APPROVED"<<endl;
+  return 0;
+
+return 0;
+}
+
