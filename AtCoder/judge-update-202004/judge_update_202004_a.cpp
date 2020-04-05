3bd8c85a1d57c6da73140b78f8f9e6fbc5ad51ad
NULLCT <cockatiel.u10@gmail.com>
2020-04-05 13:02:32 +0100

    https://atcoder.jp/contests/judge-update-202004/submissions/11581692

---
+++ AtCoder/judge-update-202004/judge_update_202004_a.cpp
@@ -0,0 +1,38 @@
+/*//////////////////////////////////////*/
+/*/  NULLCT$ (｀･ω･´) <<"Be accept!!"  /*/
+/*//////////////////////////////////////*/
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <type_traits>
+#include <cstdint>
+#include <math.h>
+#include <queue>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#define rep(counter, limit) for (int counter = 0; counter < limit; counter++)
+typedef long long ll; using namespace std;
+int gcd(int a, int b) { if (a % b == 0) { return(b); } else { return(gcd(b, a % b)); } }
+int lcm(int a, int b) { return a * b / gcd(a, b); }
+bool primenum(int a) { if (a == 1) { return false; }if (a == 2) { return true; }if (a % 2 == 0) { return false; }for (int i = 3; i < sqrt(a) + 1; i += 2) { if (a != i) { if (a % i == 0) { return false; } } }return true; }
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+  int s,l,r;
+  cin>>s>>l>>r;
+  if(l<=s && r >= s){
+    cout<<s<<endl;
+  }else{
+    if(s < l){
+      cout<<l<<endl;
+    }else{
+      if(s > r){
+        cout<<r<<endl;
+      }
+    }
+  }
+
+return 0;
+}
+
