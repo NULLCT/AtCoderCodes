d1d0daf63b650bcc980f377cfa2a2450d0d1e790
NULLCT <cockatiel.u10@gmail.com>
2020-08-15 13:02:15 +0100

    https://atcoder.jp/contests/abc175/submissions/15908918

---
+++ AtCoder/abc175/abc175_a.cpp
@@ -0,0 +1,90 @@
+//////////////////////////
+//      _        ____   //
+//  U  /"\  u U /"___|  //
+//   \/ _ \/  \| | u    //
+//   / ___ \   | |/__   //
+//  /_/   \_\   \____|  //
+//   \\    >>  _// \\   //
+//  (__)  (__)(__)(__)  //
+//  Compro by NULL_CT©  //
+//////////////////////////
+// STL libs
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cctype>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define REP(I, LIM) for (int I = 0; I < (LIM); I++)
+#define ALL(LIST) (LIST.begin()), (LIST.end())
+
+using namespace std;
+
+constexpr int MOD = 10000007;
+constexpr long INF = LONG_MAX;
+
+vector<long> divisor(long n) {
+  vector<long> head, tail;
+  for (long i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      head.push_back(i);
+      if (i * i != n)
+        tail.push_back(n / i);
+    }
+  }
+  head.insert(head.end(), tail.rbegin(), tail.rend());
+  return (head);
+}
+
+struct init {
+  init() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    // cout<<fixed<<setprecision(20);
+  }
+} init;
+
+/*----------------------------*/
+
+int main() {
+  string s;
+  cin>>s;
+
+  int ans=0;
+  if(s[0] == 'R' or s[1] == 'R' or s[2] == 'R'){
+    ans=1;
+  }
+  if(s[0] == 'R' and s[1] == 'R'){
+    ans =2;
+  }
+  if(s[1] == 'R' and s[2]=='R'){
+    ans=2;
+  }
+  if(s[0] == 'R' and s[1] == 'R' and s[2] == 'R'){
+    ans=3;
+  }
+  cout<<ans<<endl;
+}
+
