b12922fa4a30d8cecb989ea0fa38e7d681814d8d
NULLCT <cockatiel.u10@gmail.com>
2020-09-21 08:12:02 +0100

    https://atcoder.jp/contests/abc153/submissions/16932850

---
+++ AtCoder/abc153/abc153_d.cpp
@@ -0,0 +1,108 @@
+//////////////////////////
+//      _        ____   //
+//  U  /"\  u U /"___|  //
+//   \/ _ \/  \| | u    //
+//   / ___ \   | |/__   //
+//  /_/   \_\   \____|  //
+//   \\    >>  _// \\   //
+//  (__)  (__)(__)(__)  //
+//  Compro by NULL_CT©  //
+//////////////////////////
+// STL libs
+#include <algorithm>
+#include <array>
+#include <bits/stdint-intn.h>
+#include <bitset>
+#include <cctype>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// Boost
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/range/irange.hpp>
+
+#define ALL(LIST) (LIST.begin()), (LIST.end())
+
+using namespace std;
+using boost::irange;
+using boost::multiprecision::cpp_int;
+
+constexpr int64_t MOD = 1000000007;
+
+vector<int64_t> divisor(const int64_t &_n) {
+  vector<int64_t> head, tail;
+  for (int64_t i = 1; i * i <= _n; i++) {
+    if (_n % i == 0) {
+      head.push_back(i);
+      if (i * i != _n)
+        tail.push_back(_n / i);
+    }
+  }
+  head.insert(head.end(), tail.rbegin(), tail.rend());
+  return head;
+}
+
+int64_t kadanes(const vector<int64_t> &_ls) {
+  int64_t highestMax = 0, currentMax = 0, length = _ls.size();
+  for (int64_t i = 0; i < length; i++) {
+    currentMax = max(_ls[i], currentMax + _ls[i]);
+    highestMax = max(highestMax, currentMax);
+  }
+  return highestMax;
+}
+
+void execution();
+
+int main() {
+#ifndef DEBUG
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+#endif
+  // cout<<fixed<<setprecision(20);
+
+  execution();
+  return 0;
+}
+
+//--------------------------------------------------------------
+
+void execution() {
+  int64_t h,num=1,ans=0;
+  cin>>h;
+
+  while(true){
+    if(h == 1){
+      ans+=num;
+      cout<<ans<<"\n";
+      return;
+    }
+    h/=2;
+    ans+=num;
+    num*=2;
+  }
+}
+
