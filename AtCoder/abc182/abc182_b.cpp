313319b7acb876fdd3d3acfd6f951a676483749d
NULLCT <cockatiel.u10@gmail.com>
2020-11-08 21:10:01 +0900

    https://atcoder.jp/contests/abc182/submissions/17958848

---
+++ AtCoder/abc182/abc182_b.cpp
@@ -0,0 +1,146 @@
+//////////////////////////
+//      _        ____   //
+//  U  /"\  u U /"___|  //
+//   \/ _ \/  \| | u    //
+//   / ___ \   | |/__   //
+//  /_/   \_\   \____|  //
+//   \\    >>  _// \\   //
+//  (__)  (__)(__)(__)  //
+//  Compro by NULLCT   //
+//////////////////////////
+// STL libs
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <cinttypes>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// Boost
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/range/irange.hpp>
+
+#define ALL(LIST) (LIST.begin()), (LIST.end())
+
+using namespace std;
+using boost::irange;
+using boost::multiprecision::cpp_int;
+
+constexpr long MOD = 1000000007;
+
+class UnionFind {
+public:
+  vector<long> par;
+
+  UnionFind(const long _n) : par(_n, -1) {}
+  long root(const long _n) {
+    if (par[_n] < 0)
+      return _n;
+    else
+      return par[_n] = root(par[_n]);
+  }
+  bool unite(const long _main, const long _sub) {
+    long mainroot = root(_main);
+    long subroot = root(_sub);
+    if (mainroot == subroot)
+      return false;
+    par[mainroot] += par[subroot];
+    par[subroot] = mainroot;
+    return true;
+  }
+  bool isSame(const long _x,const long _y) { return root(_x) == root(_y); }
+  long size(const long _n) { return -par[root(_n)]; }
+};
+
+vector<long> divisor(const long _n) {
+  vector<long> head, tail;
+  for (long i = 1; i * i <= _n; i++) {
+    if (_n % i == 0) {
+      head.push_back(i);
+      if (i * i != _n)
+        tail.push_back(_n / i);
+    }
+  }
+  head.insert(head.end(), tail.rbegin(), tail.rend());
+  return head;
+}
+
+long kadanes(vector<long>::iterator _begin,const vector<long>::iterator _end) {
+  long highestMax = 0, currentMax = 0;
+  for (;_begin < _end;_begin++) {
+    currentMax = max(*_begin, currentMax + *_begin);
+    highestMax = max(highestMax, currentMax);
+  }
+  return highestMax;
+}
+
+void execution();
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(10);
+
+  execution();
+  return 0;
+}
+
+#define int long
+//--------------------------------------------------------------
+int gcd(int x, int y) {
+  if (x % y == 0) {
+    return y;
+  } else {
+    return gcd(y, x % y);
+  }
+}
+inline void execution() {
+  int n;
+  cin>>n;
+  vector<int> a(n);
+  for (int &i : a) {
+    cin>>i;
+  }
+
+  int ans=0;
+  int ansnum=0;
+  for (int i = 2; i <= 1000; i++) {
+    int sum=0;
+    for (int j : a) {
+      if (j % i == 0)
+        sum++;
+    }
+    if (ans < sum) {
+      ans = sum;
+      ansnum=i;
+    }
+  }
+  cout<<ansnum<<"\n";
+}
+
