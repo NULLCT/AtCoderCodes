69a84101ca5ee92cb9916efc83e90f1d81720393
NULLCT <cockatiel.u10@gmail.com>
2020-11-14 04:57:18 +0000

    https://atcoder.jp/contests/abc181/submissions/18087316

--- AtCoder/abc181/abc181_a.cpp
+++ AtCoder/abc181/abc181_a.cpp
@@ -1,128 +1,25 @@
-//////////////////////////
-//      _        ____   //
-//  U  /"\  u U /"___|  //
-//   \/ _ \/  \| | u    //
-//   / ___ \   | |/__   //
-//  /_/   \_\   \____|  //
-//   \\    >>  _// \\   //
-//  (__)  (__)(__)(__)  //
-//  Compro by NULLCT   //
-//////////////////////////
-// STL libs
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cctype>
-#include <chrono>
-#include <cinttypes>
-#include <climits>
-#include <cmath>
-#include <complex>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <deque>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <thread>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
+use std::io::*;
+use std::str::FromStr;
 
-// Boost
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/range/irange.hpp>
+pub fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed reading char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed parsing")
+}
 
-#define ALL(LIST) (LIST.begin()), (LIST.end())
-#ifdef DEBUG
-#define cerr cout
-#endif
+fn main() {
+    let n:usize = read();
 
-using namespace std;
-using boost::irange;
-using boost::multiprecision::cpp_int;
-
-constexpr long MOD = 1000000007;
-
-class UnionFind {
-public:
-  vector<long> par;
-
-  UnionFind(long _n) : par(_n, -1) {}
-  long root(long _n) {
-    if (par[_n] < 0)
-      return _n;
-    else
-      return par[_n] = root(par[_n]);
-  }
-  bool unite(long _main, long _sub) {
-    long mainroot = root(_main);
-    long subroot = root(_sub);
-    if (mainroot == subroot)
-      return false;
-    par[mainroot] += par[subroot];
-    par[subroot] = mainroot;
-    return true;
-  }
-  bool isSame(long _x, long _y) { return root(_x) == root(_y); }
-  long size(long _n) { return -par[root(_n)]; }
-};
-
-vector<long> divisor(const long _n) {
-  vector<long> head, tail;
-  for (long i = 1; i * i <= _n; i++) {
-    if (_n % i == 0) {
-      head.push_back(i);
-      if (i * i != _n)
-        tail.push_back(_n / i);
+    if n % 2 == 0 {
+        println!("White");
+    } else {
+        println!("Black");
     }
-  }
-  head.insert(head.end(), tail.rbegin(), tail.rend());
-  return head;
-}
-
-long kadanes(const vector<long> &_n) {
-  long highestMax = 0, currentMax = 0, length = _n.size();
-  for (long i = 0; i < length; i++) {
-    currentMax = max(_n[i], currentMax + _n[i]);
-    highestMax = max(highestMax, currentMax);
-  }
-  return highestMax;
-}
-
-void execution();
-
-int main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << fixed << setprecision(10);
-
-  execution();
-  return 0;
-}
-
-#define int long
-//--------------------------------------------------------------
-inline void execution() {
-  int a;
-  cin>>a;
-
-  if (a % 2 == 1) {
-    cout<<"Black\n";
-  } else {
-    cout<<"White\n";
-  }
 }
 
