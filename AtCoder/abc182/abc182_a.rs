6ece4205f4939025e07786b6feda469fd003ff35
NULLCT <cockatiel.u10@gmail.com>
2020-11-14 13:55:20 +0900

    https://atcoder.jp/contests/abc182/submissions/18087286

---
+++ AtCoder/abc182/abc182_a.rs
@@ -0,0 +1,22 @@
+use std::io::*;
+use std::str::FromStr;
+
+pub fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed reading char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed parsing")
+}
+
+fn main() {
+    let a: usize = read();
+    let b: usize = read();
+
+    println!("{}",(2*a)+100-b);
+}
+
