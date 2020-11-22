9eee3d44b0af9637a856d4cb21008ce0c8ce8be7
NULLCT <cockatiel.u10@gmail.com>
2020-11-22 21:00:36 +0900

    https://atcoder.jp/contests/abc184/submissions/18303122

---
+++ AtCoder/abc184/abc184_a.cpp
@@ -0,0 +1,187 @@
+//////////////////////////
+//      _        ____   //
+//  U  /"\  u U /"___|  //
+//   \/ _ \/  \| | u    //
+//   / ___ \   | |/__   //
+//  /_/   \_\   \____|  //
+//   \\    >>  _// \\   //
+//  (__)  (__)(__)(__)  //
+//  Compro by NULLCT   //
+//////////////////////////
+// STL libs
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <cinttypes>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// Boost
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/range/irange.hpp>
+
+#define ALL(LIST) (LIST.begin()), (LIST.end())
+
+using namespace std;
+using boost::irange;
+using boost::multiprecision::cpp_int;
+
+constexpr long MOD = 1000000007;
+
+template <long MOD> class ModNum {
+public:
+  long num;
+
+  constexpr ModNum(long v = 0) noexcept : num(v % MOD) {
+    if (num < 0) num += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr ModNum operator-() const noexcept { return num ? MOD - num : 0; }
+  constexpr ModNum operator+(const ModNum &r) const noexcept {
+    return ModNum(*this) += r;
+  }
+  constexpr ModNum operator-(const ModNum &r) const noexcept {
+    return ModNum(*this) -= r;
+  }
+  constexpr ModNum operator*(const ModNum &r) const noexcept {
+    return ModNum(*this) *= r;
+  }
+  constexpr ModNum operator/(const ModNum &r) const noexcept {
+    return ModNum(*this) /= r;
+  }
+  constexpr ModNum &operator+=(const ModNum &r) noexcept {
+    num += r.num;
+    if (num >= MOD) num -= MOD;
+    return *this;
+  }
+  constexpr ModNum &operator-=(const ModNum &r) noexcept {
+    num -= r.num;
+    if (num < 0) num += MOD;
+    return *this;
+  }
+  constexpr ModNum &operator*=(const ModNum &r) noexcept {
+    num = num * r.num % MOD;
+    return *this;
+  }
+  constexpr ModNum &operator/=(const ModNum &r) noexcept {
+    long a = r.num, b = MOD, u = 1, v = 0;
+    while (b) {
+      long t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    num = num * u % MOD;
+    if (num < 0) num += MOD;
+    return *this;
+  }
+  constexpr bool operator==(const ModNum &r) const noexcept {
+    return this->num == r.num;
+  }
+  constexpr bool operator!=(const ModNum &r) const noexcept {
+    return this->num != r.val;
+  }
+  friend constexpr ostream &operator<<(ostream &os, const ModNum<MOD> &x) noexcept {
+    return os << x.num;
+  }
+  friend constexpr ModNum<MOD> modpow(const ModNum<MOD> &a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+
+class UnionFind {
+public:
+  vector<long> par;
+
+  UnionFind(const long _n) : par(_n, -1) {}
+  long root(const long _n) {
+    if (par[_n] < 0)
+      return _n;
+    else
+      return par[_n] = root(par[_n]);
+  }
+  bool unite(const long _main, const long _sub) {
+    long mainroot = root(_main);
+    long subroot = root(_sub);
+    if (mainroot == subroot)
+      return false;
+    par[mainroot] += par[subroot];
+    par[subroot] = mainroot;
+    return true;
+  }
+  bool isSame(const long _x,const long _y) { return root(_x) == root(_y); }
+  long size(const long _n) { return -par[root(_n)]; }
+};
+
+vector<long> divisor(const long _n) {
+  vector<long> head, tail;
+  for (long i = 1; i * i <= _n; i++) {
+    if (_n % i == 0) {
+      head.push_back(i);
+      if (i * i != _n)
+        tail.push_back(_n / i);
+    }
+  }
+  head.insert(head.end(), tail.rbegin(), tail.rend());
+  return head;
+}
+
+long kadanes(vector<long>::iterator _begin,const vector<long>::iterator _end) {
+  long highestMax = 0, currentMax = 0;
+  for (;_begin < _end;_begin++) {
+    currentMax = max(*_begin, currentMax + *_begin);
+    highestMax = max(highestMax, currentMax);
+  }
+  return highestMax;
+}
+
+void execution();
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(10);
+
+  execution();
+  return 0;
+}
+
+#define int long
+//--------------------------------------------------------------
+inline void execution() {
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  cout<<a*d-b*c<<"\n";
+}
+
