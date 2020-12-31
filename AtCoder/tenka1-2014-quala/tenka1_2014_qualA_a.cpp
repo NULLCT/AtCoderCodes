9ee79076e5a99f6ef98f80126cd92119fcb0f550
NULLCT <cockatiel.u10@gmail.com>
2020-12-31 03:47:07 +0000

    https://atcoder.jp/contests/tenka1-2014-quala/submissions/19073756

--- AtCoder/tenka1-2014-quala/tenka1_2014_qualA_a.cpp
+++ AtCoder/tenka1-2014-quala/tenka1_2014_qualA_a.cpp
@@ -1,352 +1,1001 @@
-//      _        ____   //
-//  U  /"\  u U /"___|  //
-//   \/ _ \/  \| | u    //
-//   / ___ \   | |/__   //
-//  /_/   \_\   \____|  //
-//   \\    >>  _// \\   //
-//  (__)  (__)(__)(__)  //
-//  Compro by NULLCT   //
-
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC target("avx")
-
-//STL
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <cctype>
-#include <chrono>
-#include <cinttypes>
-#include <climits>
-#include <cmath>
-#include <complex>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <deque>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iostream>
-#include <iterator>
-#include <list>
-#include <map>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <thread>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
-
-//Boost
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/range/irange.hpp>
-
-#define int int64_t
-
-using namespace std;
-
-template <typename T>
-ostream &operator<<(ostream &_ostr, const vector<T> &_v);
-template <typename T>
-ostream &operator<<(ostream &_ostr, const deque<T> &_v);
-template <typename T>
-ostream &operator<<(ostream &_ostr, const list<T> &_v);
-template <typename T, typename Y>
-ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v);
-template <class... Ts>
-ostream &operator<<(ostream &_ostr, const tuple<Ts...> &t);
-template <typename T, typename Y>
-ostream &operator<<(ostream &_ostr, const map<T, Y> &_v);
-template <typename T>
-ostream &operator<<(ostream &_ostr, const set<T> &_v);
-
-template <typename T>
-ostream &operator<<(ostream &_ostr, const vector<T> &_v) {
-  _ostr << "v";
-  if (_v.empty()) {
-    _ostr << "{ }";
-    return _ostr;
-  }
-  _ostr << "{" << *_v.begin();
-  for (auto itr = ++_v.begin(); itr != _v.end(); itr++) {
-    _ostr << ", " << *itr;
-  }
-  _ostr << "}";
-  return _ostr;
-}
-template <typename T>
-ostream &operator<<(ostream &_ostr, const deque<T> &_v) {
-  _ostr << "d";
-  if (_v.empty()) {
-    _ostr << "{ }";
-    return _ostr;
-  }
-  _ostr << "{" << *_v.begin();
-  for (auto itr = ++_v.begin(); itr != _v.end(); itr++) {
-    _ostr << ", " << *itr;
-  }
-  _ostr << "}";
-  return _ostr;
-}
-template <typename T>
-ostream &operator<<(ostream &_ostr, const list<T> &_v) {
-  _ostr << "l";
-  if (_v.empty()) {
-    _ostr << "{ }";
-    return _ostr;
-  }
-  _ostr << "{" << *_v.begin();
-  for (auto itr = ++_v.begin(); itr != _v.end(); itr++) {
-    _ostr << ", " << *itr;
-  }
-  _ostr << "}";
-  return _ostr;
-}
-template <typename T, typename Y>
-ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v) {
-  _ostr << "p(" << _v.first << ", " << _v.second << ")";
-  return _ostr;
-}
-template <class... Ts>
-ostream &operator<<(ostream &_ostr, const tuple<Ts...> &_v) {
-  _ostr << "t[";
-  bool first = true;
-  apply([&_ostr, &first](auto &&... args) {
-    auto print = [&](auto &&val) {
-      if (!first)
-        _ostr << ",";
-      (_ostr << val);
-      first = false;
-    };
-    (print(args), ...);
-  },
-        _v);
-  _ostr << "]";
-  return _ostr;
-}
-template <typename T, typename Y>
-ostream &operator<<(ostream &_ostr, const map<T, Y> &_v) {
-  _ostr << "m{";
-  for (auto itr = _v.begin(); itr != _v.end(); itr++) {
-    _ostr << "(" << itr->first << ", " << itr->second << ")";
-    itr++;
-    if (itr != _v.end())
-      _ostr << ", ";
-    itr--;
-  }
-  _ostr << "}";
-  return _ostr;
-}
-template <typename T>
-ostream &operator<<(ostream &_ostr, const set<T> &_v) {
-  _ostr << "s{";
-  for (auto itr = _v.begin(); itr != _v.end(); itr++) {
-    _ostr << *itr;
-    ++itr;
-    if (itr != _v.end())
-      _ostr << ", ";
-    itr--;
-  }
-  _ostr << "}";
-  return _ostr;
-}
-
-template <int MOD>
-class ModNum {
-public:
-  int num;
-
-  constexpr ModNum(int v = 0) noexcept : num(v % MOD) {
-    if (num < 0)
-      num += MOD;
-  }
-  constexpr int getmod() { return MOD; }
-  constexpr ModNum operator-() const noexcept { return num ? MOD - num : 0; }
-  constexpr ModNum operator+(const ModNum &r) const noexcept {
-    return ModNum(*this) += r;
-  }
-  constexpr ModNum operator-(const ModNum &r) const noexcept {
-    return ModNum(*this) -= r;
-  }
-  constexpr ModNum operator*(const ModNum &r) const noexcept {
-    return ModNum(*this) *= r;
-  }
-  constexpr ModNum operator/(const ModNum &r) const noexcept {
-    return ModNum(*this) /= r;
-  }
-  constexpr ModNum &operator+=(const ModNum &r) noexcept {
-    num += r.num;
-    if (num >= MOD)
-      num -= MOD;
-    return *this;
-  }
-  constexpr ModNum &operator-=(const ModNum &r) noexcept {
-    num -= r.num;
-    if (num < 0)
-      num += MOD;
-    return *this;
-  }
-  constexpr ModNum &operator*=(const ModNum &r) noexcept {
-    num = num * r.num % MOD;
-    return *this;
-  }
-  constexpr ModNum &operator/=(const ModNum &r) noexcept {
-    int a = r.num, b = MOD, u = 1, v = 0;
-    while (b) {
-      int t = a / b;
-      a -= t * b;
-      swap(a, b);
-      u -= t * v;
-      swap(u, v);
-    }
-    num = num * u % MOD;
-    if (num < 0)
-      num += MOD;
-    return *this;
-  }
-  constexpr bool operator==(const ModNum &r) const noexcept {
-    return this->num == r.num;
-  }
-  constexpr bool operator!=(const ModNum &r) const noexcept {
-    return this->num != r.val;
-  }
-  friend constexpr ostream &operator<<(ostream &os, const ModNum<MOD> &x) noexcept {
-    return os << x.num;
-  }
-  friend constexpr ModNum<MOD> modpow(const ModNum<MOD> &a, int n) noexcept {
-    if (n == 0)
-      return 1;
-    auto t = modpow(a, n / 2);
-    t = t * t;
-    if (n & 1)
-      t = t * a;
-    return t;
-  }
-};
-
-class UnionFind {
-public:
-  int n;
-  vector<int> par;
-
-  UnionFind() : n(0) {}
-  UnionFind(int _n) : n(_n), par(_n, -1) {}
-  int merge(int a, int b) {
-    assert(0 <= a && a < n);
-    assert(0 <= b && b < n);
-    int x = root(a), y = root(b);
-    if (x == y)
-      return x;
-    if (-par[x] < -par[y])
-      swap(x, y);
-    par[x] += par[y];
-    par[y] = x;
-    return x;
-  }
-  bool isSame(int a, int b) {
-    assert(0 <= a && a < n);
-    assert(0 <= b && b < n);
-    return root(a) == root(b);
-  }
-  int root(int a) {
-    assert(0 <= a && a < n);
-    if (par[a] < 0)
-      return a;
-    return par[a] = root(par[a]);
-  }
-  int size(int a) {
-    assert(0 <= a && a < n);
-    return -par[root(a)];
-  }
-  vector<vector<int>> groups() {
-    vector<int> leader_buf(n), group_size(n);
-    for (int i = 0; i < n; i++) {
-      leader_buf[i] = root(i);
-      group_size[leader_buf[i]]++;
-    }
-    vector<vector<int>> result(n);
-    for (int i = 0; i < n; i++)
-      result[i].reserve(group_size[i]);
-    for (int i = 0; i < n; i++)
-      result[leader_buf[i]].push_back(i);
-    result.erase(remove_if(result.begin(), result.end(), [&](const vector<int> &v) { return v.empty(); }), result.end());
-    return result;
-  }
-};
-
-vector<int> divisor(const int _n) {
-  vector<int> head, tail;
-  for (int i = 1; i * i <= _n; i++) {
-    if (_n % i == 0) {
-      head.push_back(i);
-      if (i * i != _n)
-        tail.push_back(_n / i);
-    }
-  }
-  head.insert(head.end(), tail.rbegin(), tail.rend());
-  return head;
-}
-
-int kadanes(vector<int>::iterator _begin, const vector<int>::iterator _end) {
-  int highestMax = 0, currentMax = 0;
-  for (; _begin < _end; _begin++) {
-    currentMax = max(*_begin, currentMax + *_begin);
-    highestMax = max(highestMax, currentMax);
-  }
-  return highestMax;
-}
-
-int dichotomy(int ng, int ok, function<bool(int)> discriminant) {
-  while (ok - ng > 1) {
-    int mid = (ng + ok) / 2;
-    (discriminant(mid) ? ok : ng) = mid;
-  }
-  return ok;
-}
-
-void execution();
-
-signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << fixed << setprecision(10);
-
-  execution();
-  return 0;
-}
-
-#ifdef DEBUG
-#define PRINT(var) cout << #var << ": " << __func__ << " " << __LINE__ << "\n  " << var << endl;
-#else
-#define PRINT(var) ;
-#endif
-#define REP(var, lim) for (int var = 0; var < (lim); var++)
-#define FOR(var, begin, end) for (int var = (begin); var < (end); var++)
-#define ALL(var) (var).begin(), (var).end()
-#define LEN(var) (static_cast<int>(var.size()))
-
-using boost::irange;
-using boost::multiprecision::cpp_int;
-//--------------------------------------------------------------
-inline void execution() {
-  vector<string> s(1000);
-  for(int i:irange(1,1001)){
-    s[i-1] = to_string(i);
-  }
-
-  sort(ALL(s));
-
-  for(auto &i:s){
-    cout<<i<<"\n";
-  }
-}
+1
+10
+100
+1000
+101
+102
+103
+104
+105
+106
+107
+108
+109
+11
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+12
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+13
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+14
+140
+141
+142
+143
+144
+145
+146
+147
+148
+149
+15
+150
+151
+152
+153
+154
+155
+156
+157
+158
+159
+16
+160
+161
+162
+163
+164
+165
+166
+167
+168
+169
+17
+170
+171
+172
+173
+174
+175
+176
+177
+178
+179
+18
+180
+181
+182
+183
+184
+185
+186
+187
+188
+189
+19
+190
+191
+192
+193
+194
+195
+196
+197
+198
+199
+2
+20
+200
+201
+202
+203
+204
+205
+206
+207
+208
+209
+21
+210
+211
+212
+213
+214
+215
+216
+217
+218
+219
+22
+220
+221
+222
+223
+224
+225
+226
+227
+228
+229
+23
+230
+231
+232
+233
+234
+235
+236
+237
+238
+239
+24
+240
+241
+242
+243
+244
+245
+246
+247
+248
+249
+25
+250
+251
+252
+253
+254
+255
+256
+257
+258
+259
+26
+260
+261
+262
+263
+264
+265
+266
+267
+268
+269
+27
+270
+271
+272
+273
+274
+275
+276
+277
+278
+279
+28
+280
+281
+282
+283
+284
+285
+286
+287
+288
+289
+29
+290
+291
+292
+293
+294
+295
+296
+297
+298
+299
+3
+30
+300
+301
+302
+303
+304
+305
+306
+307
+308
+309
+31
+310
+311
+312
+313
+314
+315
+316
+317
+318
+319
+32
+320
+321
+322
+323
+324
+325
+326
+327
+328
+329
+33
+330
+331
+332
+333
+334
+335
+336
+337
+338
+339
+34
+340
+341
+342
+343
+344
+345
+346
+347
+348
+349
+35
+350
+351
+352
+353
+354
+355
+356
+357
+358
+359
+36
+360
+361
+362
+363
+364
+365
+366
+367
+368
+369
+37
+370
+371
+372
+373
+374
+375
+376
+377
+378
+379
+38
+380
+381
+382
+383
+384
+385
+386
+387
+388
+389
+39
+390
+391
+392
+393
+394
+395
+396
+397
+398
+399
+4
+40
+400
+401
+402
+403
+404
+405
+406
+407
+408
+409
+41
+410
+411
+412
+413
+414
+415
+416
+417
+418
+419
+42
+420
+421
+422
+423
+424
+425
+426
+427
+428
+429
+43
+430
+431
+432
+433
+434
+435
+436
+437
+438
+439
+44
+440
+441
+442
+443
+444
+445
+446
+447
+448
+449
+45
+450
+451
+452
+453
+454
+455
+456
+457
+458
+459
+46
+460
+461
+462
+463
+464
+465
+466
+467
+468
+469
+47
+470
+471
+472
+473
+474
+475
+476
+477
+478
+479
+48
+480
+481
+482
+483
+484
+485
+486
+487
+488
+489
+49
+490
+491
+492
+493
+494
+495
+496
+497
+498
+499
+5
+50
+500
+501
+502
+503
+504
+505
+506
+507
+508
+509
+51
+510
+511
+512
+513
+514
+515
+516
+517
+518
+519
+52
+520
+521
+522
+523
+524
+525
+526
+527
+528
+529
+53
+530
+531
+532
+533
+534
+535
+536
+537
+538
+539
+54
+540
+541
+542
+543
+544
+545
+546
+547
+548
+549
+55
+550
+551
+552
+553
+554
+555
+556
+557
+558
+559
+56
+560
+561
+562
+563
+564
+565
+566
+567
+568
+569
+57
+570
+571
+572
+573
+574
+575
+576
+577
+578
+579
+58
+580
+581
+582
+583
+584
+585
+586
+587
+588
+589
+59
+590
+591
+592
+593
+594
+595
+596
+597
+598
+599
+6
+60
+600
+601
+602
+603
+604
+605
+606
+607
+608
+609
+61
+610
+611
+612
+613
+614
+615
+616
+617
+618
+619
+62
+620
+621
+622
+623
+624
+625
+626
+627
+628
+629
+63
+630
+631
+632
+633
+634
+635
+636
+637
+638
+639
+64
+640
+641
+642
+643
+644
+645
+646
+647
+648
+649
+65
+650
+651
+652
+653
+654
+655
+656
+657
+658
+659
+66
+660
+661
+662
+663
+664
+665
+666
+667
+668
+669
+67
+670
+671
+672
+673
+674
+675
+676
+677
+678
+679
+68
+680
+681
+682
+683
+684
+685
+686
+687
+688
+689
+69
+690
+691
+692
+693
+694
+695
+696
+697
+698
+699
+7
+70
+700
+701
+702
+703
+704
+705
+706
+707
+708
+709
+71
+710
+711
+712
+713
+714
+715
+716
+717
+718
+719
+72
+720
+721
+722
+723
+724
+725
+726
+727
+728
+729
+73
+730
+731
+732
+733
+734
+735
+736
+737
+738
+739
+74
+740
+741
+742
+743
+744
+745
+746
+747
+748
+749
+75
+750
+751
+752
+753
+754
+755
+756
+757
+758
+759
+76
+760
+761
+762
+763
+764
+765
+766
+767
+768
+769
+77
+770
+771
+772
+773
+774
+775
+776
+777
+778
+779
+78
+780
+781
+782
+783
+784
+785
+786
+787
+788
+789
+79
+790
+791
+792
+793
+794
+795
+796
+797
+798
+799
+8
+80
+800
+801
+802
+803
+804
+805
+806
+807
+808
+809
+81
+810
+811
+812
+813
+814
+815
+816
+817
+818
+819
+82
+820
+821
+822
+823
+824
+825
+826
+827
+828
+829
+83
+830
+831
+832
+833
+834
+835
+836
+837
+838
+839
+84
+840
+841
+842
+843
+844
+845
+846
+847
+848
+849
+85
+850
+851
+852
+853
+854
+855
+856
+857
+858
+859
+86
+860
+861
+862
+863
+864
+865
+866
+867
+868
+869
+87
+870
+871
+872
+873
+874
+875
+876
+877
+878
+879
+88
+880
+881
+882
+883
+884
+885
+886
+887
+888
+889
+89
+890
+891
+892
+893
+894
+895
+896
+897
+898
+899
+9
+90
+900
+901
+902
+903
+904
+905
+906
+907
+908
+909
+91
+910
+911
+912
+913
+914
+915
+916
+917
+918
+919
+92
+920
+921
+922
+923
+924
+925
+926
+927
+928
+929
+93
+930
+931
+932
+933
+934
+935
+936
+937
+938
+939
+94
+940
+941
+942
+943
+944
+945
+946
+947
+948
+949
+95
+950
+951
+952
+953
+954
+955
+956
+957
+958
+959
+96
+960
+961
+962
+963
+964
+965
+966
+967
+968
+969
+97
+970
+971
+972
+973
+974
+975
+976
+977
+978
+979
+98
+980
+981
+982
+983
+984
+985
+986
+987
+988
+989
+99
+990
+991
+992
+993
+994
+995
+996
+997
+998
+999
 
